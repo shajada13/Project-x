1224de1aba668a277f14f8922e8db35574d8b906
ArafatEnigma <arafat.mait@gmail.com>
2025-02-25 02:23:06 +0600

    student.c updated 01

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "../include/library.h"
 #include "../include/main_menu.h"
+#include "../include/student.h"
 
 int i;
 
@@ -11,6 +12,7 @@ int main()
 {
     system("color F0");
     main_menu();
+    student();
     return 0;
 }
 
