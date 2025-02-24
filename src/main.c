e8b624889ed798185b7490daf7e019fc3aefbeb3
nisat-gg <nisathasan13@gmail.com>
2025-02-25 00:25:58 +0600

    Added stdlib.h into main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+<<<<<<< HEAD
 #include "../include/library.h"
 #include "../include/main_menu.h"
 #include "../include/student.h"
 #include "../include/room.h"
 
+=======
+
+int i;
+void moveXY(int x, int y);
+void textColor(int code);
+void resetTextColor();
+void box1();
+void mainBox();
+void main_menu();
+>>>>>>> 0fd40a6 (Added stdlib.h into main.c)
 
 // ========== Main Function ==========
 
